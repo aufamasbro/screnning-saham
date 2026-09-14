16e48591ec12f1aca4700ff8704503665fed04e1
aufamasbro <aufamasbro@gmail.com>
2026-09-15 02:59:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 import yfinance as yf
 import pandas as habib
@@ -15,7 +14,7 @@ if st.sidebar.button("Analisa"):
         st.error("tingkatkan lagi ketelitian anda!")
     else:
         if isinstance(data.columns, habib.MultiIndex):
-        data.columns = data.columns.get_level_values(0)
+            data.columns = data.columns.get_level_values(0)
 
         data['MA20'] = data['Close'].rolling(20).mean()
         data['MA50'] = data['Close'].rolling(50).mean()
@@ -25,32 +24,34 @@ if st.sidebar.button("Analisa"):
         data['VP'] = data['Typical_Price'] * data['Volume']
         data['VWAP60'] = data['VP'].rolling(60).sum() / data['Volume'].rolling(60).sum()
 
-
-
         df_plot = data.tail(180).copy()
         garis_tambahan = [
             auf.make_addplot(df_plot['MA20'], color='blue', width=1.2),
             auf.make_addplot(df_plot['MA50'], color='orange', width=1.2),
             auf.make_addplot(df_plot['MA5'], color='purple', width=1.2),
             auf.make_addplot(df_plot['Vol_MA50'], panel=1, color='pink', width=1.2),
-            auf.make_addplot(df_plot['VWAP60'], color='white', width=1.5)]
+            auf.make_addplot(df_plot['VWAP60'], color='white', width=1.5)
+        ]
 
         warna_lilin = auf.make_marketcolors(up='green', down='red', edge='inherit', wick='inherit', volume={'up': 'green', 'down': 'red'})
 
         Hb = auf.make_mpf_style(marketcolors=warna_lilin, facecolor='#1e222d', figcolor='white', gridcolor='grey', gridstyle='--')
 
-        auf.plot(df_plot, type='candle',
+        fig, _ = auf.plot(
+            df_plot, type='candle',
             volume=True,
             addplot=garis_tambahan,
             style=Hb,
             title=f"\nCHART BY AUF - {kode_saham}",
             figsize=(12, 7),
             panel_ratios=(3, 1), returnfig=True
+        )
 
         st.pyplot(fig)
 
+        data_clean = data.dropna(subset=['MA5', 'MA20', 'MA50', 'VWAP60'])
+        hari_ini = data_clean.iloc[-1]
 
-        hari_ini = data.iloc[-1]
         ma20_hari_ini = hari_ini['MA20'].item()
         ma50_hari_ini = hari_ini['MA50'].item()
         ma5_hari_ini = hari_ini['MA5'].item()
@@ -58,28 +59,22 @@ if st.sidebar.button("Analisa"):
         vol_rata_rata = hari_ini['Vol_MA50'].item()
         move_bandar = hari_ini['Close'].item() > hari_ini['VWAP60'].item()
 
-        data_clean = data.dropna(subset=['MA5', 'MA20', 'MA50', 'VWAP60'])
-        hari_ini = data_clean.iloc[-1]
-
         ma5 = hari_ini['MA5'].item()
         ma20 = hari_ini['MA20'].item()
         ma50 = hari_ini['MA50'].item()
         is_uptrend = ma5 > ma20 > ma50
 
-        st.subheader("📊 ANALISIS UNTUK {kode_saham}:")
+        st.subheader(f"📊 ANALISIS UNTUK {kode_saham}:")
         c1, c2, c3, c4 = st.columns(4)
-        c1.metric("Lonjakan Volume   : {'✅ Ada Breakout' if vol_hari_ini > (vol_rata_rata * 2.0) else '❌ Normal/Sepi'}")
-        c2.metric("akumulasi bandar   : {'✅ Di Atas Bandar Line (Akumulasi)' if move_bandar else '❌ Di Bawah Bandar Line (Distribusi)'}")
-
-
-        c4.metric(" ANGKA MA -> MA5: {ma5:.2f} | MA20: {ma20:.2f} | MA50: {ma50:.2f}")
-
-        c3.metric("TRIPLE MA CROSSING : {'✅ Uptrend' if is_uptrend else '❌ Downtrend'}")
+        c1.metric("Lonjakan Volume", f"{'✅ Ada Breakout' if vol_hari_ini > (vol_rata_rata * 2.0) else '❌ Normal/Sepi'}")
+        c2.metric("Akumulasi Bandar", f"{'✅ Di Atas Bandar Line (Akumulasi)' if move_bandar else '❌ Di Bawah Bandar Line (Distribusi)'}")
+        c3.metric("TRIPLE MA CROSSING", f"{'✅ Uptrend' if is_uptrend else '❌ Downtrend'}")
+        c4.metric("ANGKA MA", f"MA5: {ma5:.2f} | MA20: {ma20:.2f} | MA50: {ma50:.2f}")
 
         st.markdown("---")
         if is_uptrend and vol_hari_ini > (vol_rata_rata * 2.0) and move_bandar:
-            st.succes("SINYAL: VERY BUY 7% - 15%")
-        elif is_uptrend and vol_hari_ini > (vol_rata_rata * 2.0) :
+            st.success("SINYAL: VERY BUY 7% - 15%")
+        elif is_uptrend and vol_hari_ini > (vol_rata_rata * 2.0):
             st.info("SINYAL: BUY 1% - 5%")
         elif vol_hari_ini > (vol_rata_rata * 2.0) and move_bandar:
             st.info("SINYAL: BUY 1% - 5%")

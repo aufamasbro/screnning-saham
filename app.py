0fba04332feaa8443c2132ff131312087e339216
aufamasbro <aufamasbro@gmail.com>
2026-09-15 04:00:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="Screener Saham AUF", layout="centered")
 st.markdown(
     """
     <div style='text-align: center; padding: 12px; background-color: #1e222d; border-radius: 10px; margin-bottom: 15px;'>
-        <h3 style='color: #4CAF50; margin:0;'>🚀 SCREENER SAHAM</h3>
+        <h3 style='color: #4CAF50; margin:0;'>💸 SCREENER SAHAM</h3>
         <p style='color: #cccccc; margin:2px 0 0 0; font-size: 14px;'>Gain 7% - 15% • <b>by AUF</b></p>
     </div>
     """, 
